2808bb0c9ef0478be360e795253474b9a104d8a7
Khaw Yu Sheng <142068657+khawyusheng-hchs@users.noreply.github.com>
2024-09-10 08:19:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,32 @@
-const rateLimit = require('express-rate-limit');
 const express = require('express');
-const app = express();
+const rateLimit = require('express-rate-limit');
 const path = require('path');
-const fs = require('fs');
+const app = express();
 
-//DDOS Prevention
-// Apply the rate limiting middleware to all requests.
+// Rate limiting middleware (DDoS protection)
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
-  standardHeaders: true, // Return rate limit info in the RateLimit-* headers
-  legacyHeaders: false, // Disable the X-RateLimit-* headers
+  max: 100, // limit each IP to 100 requests per window
+  standardHeaders: true,
+  legacyHeaders: false,
 });
 app.use(limiter);
 
-// Serve static files from the current directory
-app.use(express.static(__dirname));
+// Serve static files from the 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Serve the index.html file
+// Serve index.html at root
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Serve the darkMode.js file
-app.get('/public/darkMode.js', (req, res) => {
-  const filePath = path.join(__dirname, 'public', 'darkMode.js');
-  fs.access(filePath, fs.constants.F_OK, (err) => {
-    if (err) {
-      console.error('darkMode.js file not found');
-      res.status(404).send('darkMode.js file not found');
-    } else {
-      res.sendFile(filePath);
-    }
-  });
-});
-
-// Handle 404 errors
-app.use((req, res, next) => {
-  res.status(404).sendFile('404.html', { root: __dirname });
+// Handle 404 (File not found) errors
+app.use((req, res) => {
+  res.status(404).sendFile(path.join(__dirname, '404.html'));
 });
 
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
